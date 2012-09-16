ed66a88b9b0e9147f5499d749c25bd64d998eb0d
Maxime Bury <maxime.bury@gmail.com>
2012-09-16 12:19:20 -0400

    Begin stylin'

--- web/bundles/app/js/script.js
+++ web/bundles/app/js/script.js
@@ -6,3 +6,35 @@
 jQuery(function($) {
 
 });
+
+$('.joke-knock-up').click(function() {
+    $this = $(this);
+    id = $this.attr('data-joke-id');
+    
+    $.ajax({
+        type: "POST",
+        url: "/vote/up/" + id
+    }).done(function(data) {
+        if ("ok" === data.status) {
+            $this.text('Knock up (+' + data.votes + ')');
+        } else {
+            // Nothing now ...
+        }
+    });
+});
+
+$('.joke-knock-down').click(function() {
+    $this = $(this);
+    id = $this.attr('data-joke-id');
+    
+    $.ajax({
+        type: "POST",
+        url: "/vote/down/" + id
+    }).done(function(data) {
+        if ("ok" === data.status) {
+            $this.text('Knock down (-' + data.votes + ')');
+        } else {
+            // Nothing now ...
+        }
+    });
+});
