8e3201841428c18dad4be346ff0dbe3f3ae1b7b0
Maxime Bury <maxime.bury@gmail.com>
2012-09-17 17:41:15 -0400

    Finished stylin'

--- src/meme/Resources/public/js/script.js
+++ src/meme/Resources/public/js/script.js
@@ -9,52 +9,73 @@ jQuery(function($) {
 });
 
 $('.joke-knock-up').click(function() {
-    $this = $(this);
-    id = $this.attr('data-joke-id');
+    var $this = $(this);
+    var id = $this.attr('data-joke-id');
     
     $.ajax({
         type: "POST",
         url: "/vote/up/" + id
     }).done(function(data) {
+        var topbar = $this.closest('.joke-topbar');
+        var score = $this.next();
         if ("ok" === data.status) {
-            $this.text('Knock up (+' + data.votes + ')');
+            score.text(' (+' + data.votes + ')');
+            topbar.addClass('success');
+            
+            setTimeout(function() {
+                this.topbar.removeClass('success');
+            }.bind(topbar), 3000);
         } else {
-            // Nothing now ...
+            topbar.addClass('failure');
+            
+            setTimeout(function() {
+                this.removeClass('failure');
+            }.bind(topbar), 3000);
         }
     });
 });
 
 $('.joke-knock-down').click(function() {
-    $this = $(this);
-    id = $this.attr('data-joke-id');
+    var $this = $(this);
+    var id = $this.attr('data-joke-id');
     
     $.ajax({
         type: "POST",
         url: "/vote/down/" + id
     }).done(function(data) {
+        var topbar = $this.closest('.joke-topbar');
+        var score = $this.next();
         if ("ok" === data.status) {
-            $this.text('Knock down (-' + data.votes + ')');
+            score.text(' (-' + data.votes + ')');
+            topbar.addClass('success');
+            
+            setTimeout(function() {
+                this.topbar.removeClass('success');
+            }.bind(topbar), 3000);
         } else {
-            // Nothing now ...
+            topbar.addClass('failure');
+            
+            setTimeout(function() {
+                this.removeClass('failure');
+            }.bind(topbar), 3000);
         }
     });
 });
 
-$('.joke-body').click(function() {
+$('.show-more').click(function() {
     var $this = $(this);
-    var height = $this.children().first().outerHeight();
+    var parent = $this.closest('.joke-body');
+    var height = parent.outerHeight();
     
-    $this.toggleClass('click-me dont-click-me');
-    $this.toggleClass('open closed');
+    $this.removeClass("btn");
     
-    if ($this.hasClass('closed')) {
-        $this.transition({
-            height: '30px'
+    if ($this.parent().next().hasClass('joke-punchline')) {
+        parent.transition({
+            height: $this.closest('.inner').outerHeight() + 'px'
         });
-    } else if ($this.hasClass('open')) {
-        $this.transition({
-            height: height + 'px'
-        }, height + 600);
+    } else {
+        parent.transition({
+            height: (height + 62) + 'px'
+        });
     }
 });
-
